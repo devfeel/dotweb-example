4c66131a708a0449732f0915e69d8e8f821ed043
pzrr@qq.com <pzrr@qq.com>
2017-03-27 11:18:30 +0800

    init version

---
+++ config/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"github.com/devfeel/dotweb"
+	"github.com/devfeel/dotweb/config"
+)
+
+func main() {
+	//初始化DotServer
+	app := dotweb.New()
+
+	//注册HttpHandler
+	RegisterHandler(app.HttpServer)
+
+	appConfig := config.InitConfig("d:/dotweb.conf")
+
+	fmt.Println("dotweb.StartServer => " + fmt.Sprint(appConfig))
+	err := app.StartServerWithConfig(appConfig)
+	fmt.Println("dotweb.StartServer error => ", err)
+}
+
+func Index(ctx *dotweb.HttpContext) {
+	ctx.Response.Header().Set("Content-Type", "text/html; charset=utf-8")
+	ctx.WriteString("index")
+}
+
+func DefaultError(ctx *dotweb.HttpContext) {
+	panic("my panic error!")
+}
+
+func Redirect(ctx *dotweb.HttpContext) {
+	ctx.Redirect("http://www.baidu.com")
+}
+
+func RegisterHandler(server *dotweb.HttpServer) {
+	server.Router().RegisterHandler("Index", Index)
+	server.Router().RegisterHandler("DefaultError", DefaultError)
+	server.Router().RegisterHandler("Redirect", Redirect)
+}
