2b8c95d22d2bc346b521157e86f5fa7d6e4d8b64
pzrr@qq.com <pzrr@qq.com>
2017-11-03 16:33:50 +0800

    完善cookie示例

--- cookie/main.go
+++ cookie/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"github.com/devfeel/dotweb"
+	"net/http"
+	"net/url"
 	"strconv"
 )
 
@@ -28,7 +30,7 @@ func main() {
 	app.AppContext.Set("gint", 1)
 
 	// 开始服务
-	port := 8080
+	port := 8081
 	fmt.Println("dotweb.StartServer => " + strconv.Itoa(port))
 	err := app.StartServer(port)
 	fmt.Println("dotweb.StartServer error => ", err)
@@ -40,9 +42,9 @@ type UserInfo struct {
 }
 
 func One(ctx dotweb.Context) error {
-	ctx.SetCookieValue("dotweb-test", "dotweb", 0)
+	ctx.SetCookieValue("dotweb-test", "SetCookieValue", 1000)
 	fmt.Println("One ", "dotweb")
-	_, err := ctx.WriteString("One - set cookie")
+	_, err := ctx.WriteString("One - set cookie value")
 	return err
 }
 
@@ -53,7 +55,15 @@ func Two(ctx dotweb.Context) error {
 	return err
 }
 
+func Three(ctx dotweb.Context) error {
+	ctx.SetCookie(&http.Cookie{Name: "dotweb-test", Value: url.QueryEscape("SetCookie"), MaxAge: 1000})
+	fmt.Println("Three ", "dotweb")
+	_, err := ctx.WriteString("Three - set cookie")
+	return err
+}
+
 func InitRoute(server *dotweb.HttpServer) {
-	server.Router().GET("/", One)
+	server.Router().GET("/1", One)
 	server.Router().GET("/2", Two)
+	server.Router().GET("/3", Three)
 }
