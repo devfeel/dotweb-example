637c6611f62ea11b4289faaf256194cb83331b66
pzrr@qq.com <pzrr@qq.com>
2018-10-30 15:15:53 +0800

    * New Feature: Add HttpServer.SetEnabledStaticFileMiddleware, used to set flag which enabled or disabled middleware for static-file route * Detail:   - if set ture, when visit static file route, will use middlewares like other router   - the execute order: App -> Group -> Router   - default is not enabled * Example: ``` golang app.HttpServer.SetEnabledStaticFileMiddleware(true) ``` * 2018-10-30 15:00

--- static/main.go
+++ static/main.go
@@ -4,18 +4,21 @@ import (
 	"fmt"
 	"github.com/devfeel/dotweb"
 	"github.com/devfeel/dotweb/framework/file"
+	"github.com/devfeel/middleware/gzip"
 	"strconv"
 )
 
 func main() {
 	//初始化DotServer
-	app := dotweb.New()
+	app := dotweb.Classic(file.GetCurrentDirectory())
 
+	app.SetDevelopmentMode()
 	//设置dotserver日志目录
 	app.SetLogPath(file.GetCurrentDirectory())
 
 	app.HttpServer.SetEnabledListDir(false)
-
+	app.HttpServer.SetEnabledStaticFileMiddleware(true)
+	//app.UseRequestLog()
 	//设置路由
 	InitRoute(app.HttpServer)
 
@@ -23,12 +26,22 @@ func main() {
 	//app.SetPProfConfig(true, 8081)
 
 	// 开始服务
-	port := 80
+	port := 8080
 	fmt.Println("dotweb.StartServer => " + strconv.Itoa(port))
 	err := app.StartServer(port)
 	fmt.Println("dotweb.StartServer error => ", err)
 }
 
 func InitRoute(server *dotweb.HttpServer) {
-	server.Router().ServerFile("/*filepath", "./")
+	g := server.Group("/files").Use(gzip.Middleware(gzip.NewConfig().UseDefault()))
+	g.Use(&dotweb.RequestLogMiddleware{})
+	g.ServerFile("/*filepath", "D:/gotmp")
+	server.GET("/test", func(ctx dotweb.Context) error {
+		return ctx.WriteString("test gzip")
+	}).Use(gzip.Middleware(gzip.NewConfig().UseDefault()))
+
+
+	server.GET("/test2/:name", func(ctx dotweb.Context) error {
+		return ctx.WriteString("test 2")
+	})
 }
